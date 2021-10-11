239e96db056d7d5998af31d00808a826fda25413
Inyong Tisto <imtisto@gmail.com>
2021-10-11 20:47:00 +0700

    - convert to Rupiah nullable intenger

--- MyHelper/src/main/java/com/inyongtisto/myhelper/extension/StringExtension.kt
+++ MyHelper/src/main/java/com/inyongtisto/myhelper/extension/StringExtension.kt
@@ -29,6 +29,11 @@ fun Int.toRupiah(hideCurrency: Boolean = false): String {
     return value
 }
 
+
+fun Int?.toRupiah(hideCurrency: Boolean = false): String {
+    return (this ?: 0).toRupiah(hideCurrency)
+}
+
 fun Double.toRupiah(hideCurrency: Boolean = false): String {
     val localeID = Locale("in", "ID")
     val format = NumberFormat.getCurrencyInstance(localeID)
